47b586994157573745a4ae0bc5176b1e3641c92a
Tony Bowden <tony@tmtm.com>
2016-01-14 12:59:16 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,32 +1,13 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'nokogiri'
-require 'open-uri'
 require 'pry'
-require 'rest-client'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
-end
-
-def wikinames(url)
-  noko = noko_for(url)
-  noko.xpath('//table[@class="navbox"]//table//tr[td]/td[1]//a[not(@class="new")]/@title').map(&:text)
-end
-
-names = wikinames('https://en.wikipedia.org/wiki/Template:Current_MPs_of_South_Africa')
-abort "No names" if names.count.zero?
-
-WikiData.ids_from_pages('en', names).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  ScraperWiki.save_sqlite([:id], data)
-end
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/Template:Current_MPs_of_South_Africa',
+  xpath: '//table[@class="navbox"]//table//tr[td]/td[1]//a[not(@class="new")]/@title',
+) 
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+warn EveryPolitician::Wikidata.notify_rebuilder
 
