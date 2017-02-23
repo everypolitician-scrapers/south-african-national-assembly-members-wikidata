f8b4a488785418b4b316c73abd2462995a6cb878
Tony Bowden <tony@tmtm.com>
2017-02-23 13:01:58 +0000

    Also fetch Wikidata Members of the 26th Term

--- scraper.rb
+++ scraper.rb
@@ -4,8 +4,19 @@
 require 'wikidata/fetcher'
 require 'pry'
 
+sparq = <<EOQ
+  SELECT ?item
+  WHERE {
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 wd:Q16744266 ;
+                        pq:P2937 wd:Q18109299 .
+  }
+EOQ
+
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
 names = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/Template:Current_MPs_of_South_Africa',
   xpath: '//div[@class="navbox"]//table//tr[td]/td[1]//a[not(@class="new")]/@title',
 )
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
