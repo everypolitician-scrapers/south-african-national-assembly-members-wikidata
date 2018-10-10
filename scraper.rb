d9b463b241b7ff84d2c3a6d26d6613e056ba3558
Tony Bowden <tony@tmtm.com>
2018-10-10 10:40:21 +0100

    Also fetch from Afrikaans Wikipedia
    This has 100% matches

--- scraper.rb
+++ scraper.rb
@@ -7,8 +7,14 @@ require 'pry'
 sparq = 'SELECT ?item WHERE { ?item p:P39/pq:P2937 wd:Q18109299 }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
-names = EveryPolitician::Wikidata.wikipedia_xpath(
+en_names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/Template:Current_MPs_of_South_Africa',
   xpath: '//div[@role="navigation"]//table//tr[td]/td[1]//a[not(@class="new")]/@title',
 )
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
+
+af_names = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://af.wikipedia.org/wiki/Sjabloon:Huidige_LP%27s_van_Suid-Afrika',
+  xpath: '//div[@role="navigation"]//table//tr[td]/td[1]//a[not(@class="new")]/@title',
+)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: en_names, af: af_names })
