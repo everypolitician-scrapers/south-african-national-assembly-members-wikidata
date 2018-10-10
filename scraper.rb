4bee46c96bd8a9553c03c9827ac7549adcf9b8a1
Tony Bowden <tony@tmtm.com>
2018-10-10 11:11:26 +0100

    Deal with IDs throughout, rather than names
    use the new 'as_ids' functionality from 0.22 of wikidata-fetcher

--- scraper.rb
+++ scraper.rb
@@ -2,19 +2,20 @@
 # encoding: utf-8
 
 require 'wikidata/fetcher'
-require 'pry'
 
 sparq = 'SELECT ?item WHERE { ?item p:P39/pq:P2937 wd:Q18109299 }'
-ids = EveryPolitician::Wikidata.sparql(sparq)
+wikidata = EveryPolitician::Wikidata.sparql(sparq)
 
-en_names = EveryPolitician::Wikidata.wikipedia_xpath(
+enwiki = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/Template:Current_MPs_of_South_Africa',
   xpath: '//div[@role="navigation"]//table//tr[td]/td[1]//a[not(@class="new")]/@title',
+  as_ids: true,
 )
 
-af_names = EveryPolitician::Wikidata.wikipedia_xpath(
+afwiki = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://af.wikipedia.org/wiki/Sjabloon:Huidige_LP%27s_van_Suid-Afrika',
   xpath: '//div[@role="navigation"]//table//tr[td]/td[1]//a[not(@class="new")]/@title',
+  as_ids: true,
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: en_names, af: af_names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: wikidata | enwiki | afwiki)
